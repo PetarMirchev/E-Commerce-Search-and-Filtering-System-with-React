4a5ce02e1d1f44db89f89648efbc3debb9e0004f
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-04-20 15:50:12 +0300

    style the navbar
    add staff & style navbar

--- client/src/Navbar/Navbar.jsx
+++ client/src/Navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./navbar.css";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="nav-div">
+      <div className="nav-container">
+        <input type="text" name="find" value="" placeholder='Enter your search...' className="search-input"/>
+      </div>
+      <div className="profile-container">
+        <a href="#"><FiHeart className="nav-icons"/></a>
+        <a href="#"><AiOutlineShoppingCart className="nav-icons"/></a>
+        <a href="#"><AiOutlineUserAdd className="nav-icons"/></a>
+      </div>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
