396eed35354d046c2c958c10870f3228e9d6efef
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-04-20 16:59:01 +0300

    add product cart + top product btns
    add product cart box and top recommended product by brand

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
       <>
         <Navbar/>
-        <Products/>
         <Recommended/>
+        <Products/>
       </>
   );
 }
--- client/src/Recommended/Recommended.jsx
+++ client/src/Recommended/Recommended.jsx
@@ -3,7 +3,19 @@ import "./recommended.css";
 
 const Recommended = () => {
   return (
-    <div>Recommended</div>
+    <>
+    <div>
+      <h2 className="recommended-title">Recommended</h2>
+      <div className="recommended-flex">
+        <button className='btns'>All Products</button>
+        <button className='btns'>Nike</button>
+        <button className='btns'>Adidas</button>
+        <button className='btns'>Puma</button>
+        <button className='btns'>Kappa</button>
+        <button className='btns'>Runners</button>
+      </div>
+    </div>
+    </>
   )
 }
 
