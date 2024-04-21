b61a32ea5818e30428bbcefe135999ea857780ba
Petar Mirchev <petar.g.mirchev@gmail.com>
2024-04-21 14:25:24 +0300

    style options for Colors & Price renge
    add style and renge for Colors & Price

--- client/src/Sidebar/Colors/Colors.jsx
+++ client/src/Sidebar/Colors/Colors.jsx
@@ -4,7 +4,43 @@ import "./colors.css";
 
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div className='ml'>
+      <h2 className='sidebar-title colors-title'>Colors</h2>
+
+      <div className='box-wrapper'>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test3' value=''/>
+          <span className='checkmark'></span>All
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test3' value=''/>
+          <span className='checkmark'></span>Black
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test3' value=''/>
+          <span className='checkmark'></span>Blue
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test3' value=''/>
+          <span className='checkmark'></span>Red
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test3' value=''/>
+          <span className='checkmark'></span>Green
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test3' value=''/>
+          <span className='checkmark'></span>White
+        </label>
+
+        </div>
+    </div>
   )
 }
 
--- client/src/Sidebar/Price/Price.jsx
+++ client/src/Sidebar/Price/Price.jsx
@@ -3,7 +3,38 @@ import "./price.css";
 
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className='sidebar-title price-title'>Price</h2>
+
+      <div className='box-wrapper'>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test2' value=''/>
+          <span className='checkmark'></span>$0 - $50
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test2' value=''/>
+          <span className='checkmark'></span>$51 - $100
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test2' value=''/>
+          <span className='checkmark'></span>$101 - $150
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test2' value=''/>
+          <span className='checkmark'></span>$151 - $200
+        </label>
+
+        <label className='sidebar-label-container'>
+          <input type="radio" name='test2' value=''/>
+          <span className='checkmark'></span>Over $200
+        </label>
+
+        </div>
+    </div>
   )
 }
 
